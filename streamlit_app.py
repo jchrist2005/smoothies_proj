d34140a24debf825ea22b34fc000be56eed29a97
jchrist2005 <136829879+jchrist2005@users.noreply.github.com>
2024-05-20 18:06:27 -0400

    Update streamlit_app.py
    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ ingredients_list = st.multiselect(
 ingredients_string = ''
 if ingredients_list:
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit + ', '
+        ingredients_string += fruit_chosen + ', '
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         # st.text(fruityvice_response.json())
