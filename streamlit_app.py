ed6c1af17b29b2bec93827d333f35c239d597cbe
jchrist2005 <136829879+jchrist2005@users.noreply.github.com>
2024-05-20 18:03:08 -0400

    Update streamlit_app.py
    adding specific fruit example

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ ingredients_string = ''
 if ingredients_list:
     for fruit in ingredients_list:
         ingredients_string += fruit + ', '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit)
         # st.text(fruityvice_response.json())
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         # st.write(ingredients_string)
