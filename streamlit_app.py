7a4c627c81dcd7b9ac3d60b1988848d67c1e79d0
jchrist2005 <136829879+jchrist2005@users.noreply.github.com>
2024-05-20 16:30:16 -0400

    Update streamlit_app.py
    adding df

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,6 @@ if time_to_insert:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+# st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), user_container_width=true)
 
