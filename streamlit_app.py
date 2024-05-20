6a8b3ff1daaac8abf48e55535cd6c86672bff08c
jchrist2005 <136829879+jchrist2005@users.noreply.github.com>
2024-05-20 16:33:11 -0400

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ if time_to_insert:
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), user_container_width=True)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
